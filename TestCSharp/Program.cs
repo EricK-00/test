6bab7d2097bbe9ccad44ed78f3f15862cdebe151
EricK-00 <carter000@naver.com>
2023-01-05 11:22:37 +0900

    [Gamma] (feat) make new method

---
+++ TestCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TestCSharp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			Ruler ruler = new Ruler(10);
+			ruler.PrintInch();
+		}
+	}
+
+	class Ruler
+	{
+		private const float ONE_INCH = 2.54f;
+		public int Centimeter { get; set; } = 0;
+		public float Inch
+		{
+			get { return Centimeter * ONE_INCH; }
+		}
+
+		public Ruler(int centimeter) { Centimeter = centimeter; }
+
+		public void PrintInch()
+		{
+			Console.WriteLine($"{Centimeter}cm는 {Inch}inch 입니다.");
+		}
+
+		//	2023.01.05. make new method
+		private void SetInch(int inch)
+		{
+			Centimeter = (int)(inch / ONE_INCH);
+		}
+	}
+}
