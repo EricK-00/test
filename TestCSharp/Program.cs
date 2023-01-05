7b378275d3b771019db95cda3571db91e0e90ae4
EricK-00 <carter000@naver.com>
2023-01-05 09:46:55 +0900

    [EricK] New Program

---
+++ TestCSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TestCSharo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			Ruler ruler = new Ruler(10);
+			ruler.PrintInch();
+		}
+	}
+
+	class Ruler
+	{
+		private const float ONE_INCH = 2.54f;
+		public int Centimeter { get; set; } = 0;
+		public float Inch
+		{
+			get { return Centimeter * ONE_INCH; }
+			private set { Centimeter = (int)(value / ONE_INCH); }
+		}
+
+		public Ruler(int centimeter) { Centimeter = centimeter; }
+
+		public void PrintInch()
+		{
+			Console.WriteLine($"{Centimeter}cm는 {Inch}inch 입니다.");
+		}
+	}
+}
